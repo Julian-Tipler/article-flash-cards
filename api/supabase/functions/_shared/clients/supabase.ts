3b5ee93e3821101e47efe392c194456d05916964
Julian <tipler.julian@gmail.com>
2024-04-12 17:26:47 -0500

    temporary type removal

--- api/supabase/functions/_shared/clients/supabase.ts
+++ api/supabase/functions/_shared/clients/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
 // import type { Database } from "../../types/supabase.ts";
 
-export const supabase = createClient<Database>(
+// export const supabase = createClient<Database>
+export const supabase = createClient(
   // https://github.com/orgs/supabase/discussions/14169 best practice for connecting to supabase API
   Deno.env.get("SUPABASE_URL") ?? "",
   Deno.env.get("KEY") ?? "",
