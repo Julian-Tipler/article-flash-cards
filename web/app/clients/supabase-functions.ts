7507ae4d04eb83e35f2030f6e6535b710a907345
Julian <tipler.julian@gmail.com>
2024-04-13 12:10:37 -0500

    set up oauth google env vars

--- web/app/clients/supabase-functions.ts
+++ web/app/clients/supabase-functions.ts
@@ -2,7 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_FUNCTIONS_URL || "";
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_API_TOKEN || "";
-console.log("supabaseUrl", supabaseUrl);
-console.log("supabaseAnonKey", supabaseAnonKey);
 
 export const supabaseFunctions = createClient(supabaseUrl, supabaseAnonKey);
