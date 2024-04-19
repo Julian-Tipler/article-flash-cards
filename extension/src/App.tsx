f2423dfcf50087cfac277edcd410333fcef84fe3
Julian <tipler.julian@gmail.com>
2024-04-19 17:26:15 -0500

    !!! removed auth routes for extension popup

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -2,12 +2,12 @@ import {
   RouterProvider,
   createHashRouter,
   Outlet,
-  redirect,
+  // redirect,
 } from "react-router-dom";
 import { Login } from "./views/auth/Login";
-import { MainLayout } from "./MainLayout";
-import { Session } from "@supabase/supabase-js";
-import { supabase } from "./clients/supabase";
+// import { MainLayout } from "./MainLayout";
+// import { Session } from "@supabase/supabase-js";
+// import { supabase } from "./clients/supabase";
 
 const App = () => {
   const router = createHashRouter([
@@ -21,49 +21,13 @@ const App = () => {
       ),
       children: [
         {
-          path: "/",
-          element: <MainLayout />,
-          loader: protectedLoader,
-        },
-        {
-          path: "/login",
+          index: true,
           element: <Login />,
-        },
-        {
-          path: "*",
-          element: <div>Error</div>,
         },
       ],
     },
   ]);
   return <RouterProvider router={router} fallbackElement={<></>} />;
-};
-
-async function protectedLoader() {
-  const sessionToken = await isAuthenticated();
-  if (!sessionToken) {
-    return redirect("/login");
-  }
-  if (!sessionToken || !sessionToken.access_token) {
-    return redirect("/login");
-  }
-
-  const auth = await supabase.auth.getUser(sessionToken.access_token);
-  if (!auth.data?.user) {
-    return redirect("/login");
-  }
-  return { sessionToken };
-}
-const isAuthenticated = async (): Promise<Session | false> => {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(["wiseSessionToken"], function(result) {
-      if (result.wiseSessionToken) {
-        resolve(result.wiseSessionToken);
-      } else {
-        resolve(false);
-      }
-    });
-  });
 };
 
 export default App;
